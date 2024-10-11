1ef16f8772d83d80a151e0e84368e7ad0798cd16
inkee <inkee1234@gmail.com>
2024-10-10 21:23:55 -0400

    jokes

---
+++ cogs/jokes.py
@@ -0,0 +1,42 @@
+from discord.ext import commands
+import requests
+
+class jokes(commands.Cog, name="jokes"):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @commands.hybrid_command(name="dadjoke", description="want a dad joke?")
+    async def dad_joke(self, ctx: commands.Context):
+        
+        # Fetch a joke
+        url = f"https://icanhazdadjoke.com/"
+        headers = {
+            "Accept": "text/plain"
+        }
+        
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            await ctx.send(response.content.decode())
+        else:
+            await ctx.send("Could not find a joke. Please try again.")
+
+    @commands.hybrid_command(name="randomjoke", description="want a random joke?")
+    async def random_joke(self, ctx: commands.Context):
+        
+        # Fetch a joke
+        url = f"https://official-joke-api.appspot.com/random_joke"
+        
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            setup = data["setup"]
+            punchline = data["punchline"]
+            await ctx.send(f"## {setup}\n||{punchline}||")
+        else:
+            await ctx.send("Could not find a joke. Please try again.")
+
+async def setup(bot) -> None:
+    await bot.add_cog(jokes(bot))
